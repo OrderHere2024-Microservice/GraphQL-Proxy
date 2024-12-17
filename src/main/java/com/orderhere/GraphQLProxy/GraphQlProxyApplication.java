5e2824b095fdc03ca9185b4f1d4e9109b7541668
SteveGuoh <hghandle@outlook.com>
2024-12-17 20:15:18 +1100

    Init repo

---
+++ src/main/java/com/orderhere/GraphQLProxy/GraphQlProxyApplication.java
@@ -0,0 +1,13 @@
+package com.orderhere.GraphQLProxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphQlProxyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraphQlProxyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/orderhere/GraphQLProxy/GraphQlProxyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orderhere.GraphQLProxy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphQlProxyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
